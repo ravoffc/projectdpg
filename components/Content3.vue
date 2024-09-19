17ecf329f5dcb69b67671f2f85549aee72b1cf8f
rav_official <97645551+ravoffc@users.noreply.github.com>
2024-09-19 10:09:32 +0700

    Update Content3.vue

--- components/Content3.vue
+++ components/Content3.vue
@@ -4,7 +4,7 @@
 <div class="movie_card" id="bright">
   <div class="info_section">
     <div class="movie_header">
-      <img class="locandina" src="https://previews.dropbox.com/p/thumb/ACVhowUwgRiNHEJ0I4FMrMJjkGVBA88_iUcR_AKRSty752232y_0t_UziZTvu8pHo7K-9a99N4x4GRtOKwKyZDV-1H_Ig62lsfY_eDGX6MskaHv_GLEZ-iQtFV2v0_7VM2X8B-MiOESn6_12YmNPEJ6mosjL4x_6OJqDpHGm7XXk4TF2A6e2-hNBpgyF8f5G_W7esi-IRbxlbgYuj1caO6PxIVwJwp7rO0qJMlGS7xJpR5uGiZaF52xE_j6wYyW8-Brk-4OtVJg4f63ci2z4Ggzdxw0oi6ea4VnoQjOZdEMeBPy_s6TvUzhcib7r05W6fvhYogYNyAPjwW5HgToPrOS3Zuml02VA4jQ5aCC2x5gxNpInEP2yRCTsZAhZXzmXgVE/p.png"/>
+      <img class="locandina" src="~/static/assets/kss/cv2.png"/>
       <h1>Selotip</h1>
       <h4>2023, PusaranKata</h4>
       <span class="minutes">14 min</span>
@@ -29,7 +29,7 @@
 <div class="movie_card" id="tomb">
   <div class="info_section">
     <div class="movie_header">
-      <img class="locandina" src="https://previews.dropbox.com/p/thumb/ACWlSZ2kWaudazmPeuQ6HX6YiEiyL83aS6kSnjIt3HYEBieAJpyeWrhW3YD7r1Yx3dmQcetF69DjCLkI2me7qLxcedXvUxcIXa8F2VVOfbrqQFeLhdB3T0omQf_HJAYBqCrHTzEES_llVu6ZL7JLdlxZQDS5fb78XYn9hML3BNQf4hAEOngy9YofPMXCetQhLx5GuNwPArkGrmhT85in9-KvOCLaC7fK5DbauQkMEExMa1zmsEgpJgOiwLI3x8pNKEQCssFNO9gvEXlrPNrGQ4YPPDuX-CU4SuzLLk5vHAGZLiuG4BLQum3jf3E4yfRfsYV7jZf4hOz7U44bFKXm8rQswq_IRQSV-AJPLI8i87RgiFRdnrhUDzDWrZh-Gdr02OM/p.png"/>
+      <img class="locandina" src="~/static/assets/kss/cv4.png"/>
       <h1>Sabar</h1>
       <h4>2023, PusaranKata</h4>
       <span class="minutes">14 min</span>
@@ -54,7 +54,7 @@
 <div class="movie_card" id="ave">
   <div class="info_section">
     <div class="movie_header">
-      <img class="locandina" src="https://previews.dropbox.com/p/thumb/ACXYXge_QfT97xg2_fkQNFjRBRlpbWIAYdMolYxpwGzd72DtgIPymjqvFh52yl1Jk19gC4majMCwWr1Z1-PUXmP7FO-XWl8aGrXmqs45JSRvpynie_WbTfYEIf0jMHMtglOnf6_gv-8MJADQgJpXca4_XVX2WuFWTjaFUU-c7jpwqIZnsuQLBUKChPPoSXcTEp0vV2CTA2PKGhL13wHXxewxxlkXSdpPtM-7FGTU1Q7UuwJq29BC0-Z_h99uKodjmzTvtTU1R02mZxrQt_BxOqa5Y8tjv-ZJDhyTuGwuhscm0tBmiMsgRGonHOCvdnIUSCYV4IedJZWe_DAc_c4nOiDNoI--M3LZUU2RquW-0Fled27HS13xgWFaLO-1FwyOLu8/p.png"/>
+      <img class="locandina" src="~/static/assets/kss/cv3.png"/>
       <h1>TFQ</h1>
       <h4>2024, Natzyv</h4>
       <span class="minutes">33 sec</span>
@@ -241,14 +241,14 @@ export default {
 
 
 .bright_back{
-  background: url("https://previews.dropbox.com/p/thumb/ACUyxFn70rth7Fggzfkl7EM83QbrylgXsa_iae6uOd0Oj-hgIsQ9MKKczbZn4PjQoiKHHOn9DqlnWlQdGXdFFN5UuDHR6DfctA0ucfgmqEL1tm7rOhkK14aI8aJ55z50-zGJVN5-zB4EIzgUuD4zS2GcNpvFxWAtn45g2rXc_POn7lkh0Een3URucx-z37mcblDzbWCqi3UZTfRl5gDhhRaePkS7nfUfvS0bocYxPRs_5vcdzk8vjxYU1lYILQ52QBFfYlM0XJNlwGSNM3ltmEKVqso0HFwZLEfTI9Uam2_eCutV-Z94bPt_zjDCEWgLknkh9lp_i3X3toAGID73OaGs4H5y9k7U8siDudf_0ioSOvwKMGw3L_oIxnDpjLNYCOY/p.jpeg");
+  background: url("~/static/assets/kss/pst(1).jpg");
 }
 
 .tomb_back{
-  background: url("https://previews.dropbox.com/p/thumb/ACWI49UEyIjlpd2nbjqMx9L9MQB3OVpvuc-8KkWeCjyxntGDy9awE2fsQ3jXuiQygHnuRGSlSRbEkNZEh7dATZSJFbQIkcVGOv0b3Lp-ZxNLykIVH3c3wKDsrJRDzWoH2lM67g5S3muGK1Nuf4yuMsiuHDUduq5xaSupUnDN3QHc2sxnvgQG2bLQBd4jGfX-U2iFDfUtgoU_8I0SlJCQ1SD3PX0XsLXmWf204N1A06cTbqqz5wKq8fPX4Y8djbLGiSM2HdtYgdFPs7bUzeFAperlpSE-8QZZgQmVoTvufgC2Kx-P0kKMI_4h_VQC_STrUmBZ6kgZbhjCTJaYOOPtOYxlzAka9PJ0hd0rDtMcXcw8BholJYgKRw9VeLFrxw17SEA/p.jpeg");
+  background: url("~/static/assets/kss/pst(2).jpg");
 }
 
 .ave_back{
-    background: url("https://previews.dropbox.com/p/thumb/ACWNdPMTEvVs6SlLDEh6xZQQ5nUMCngmSCdLKdFOMhkIlFq_zI8MNq648Eo_S-HOTiReng6QUQoMIlBtgzu3D4XdqZDp78xJ1YqCQG67XGFi_74B1UW2m_gBhnFdOAuDjdgw3UOIBKLjao82vQHpAZ0lDtIoqI1FX8K6a0_nXxaMkJqCIKleyP4xYlcwQvydbYT1oxC2yodAqHlNuT2EHfY6wUgTHBsJ0zcP0ZUCZBUOJPbN4mp6Ms7lhRlb2DRb8-anEUixmIL6a1Eb4fVE0WllCh-b1KuE8m6Stz9MNiC2Rs64HtVHU28tKUFOdqTuiUS3CJm4l0qym_FG23OaCMfPznD48Xhr0A5IKBeKUmwx6wSRIOubBlNDxSJY9zvVPh-Ch4UXfGR7Y7zqS-qvDuEVkaSyq9fzVf1z1WKlHIk_5Ok1IedD6Ir6z6s68_QKOCpwXLt1v9Lp_MA0DLNPMK4PgsgUtxpMMzc-UPo_rPb4TZMPP1Bx6z_dSsKoVbSFtFw/p.png");
+    background: url("~/static/assets/kss/cv1.png");
 }
 </style>
